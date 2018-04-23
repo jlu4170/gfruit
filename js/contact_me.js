ef333c044f22e3a31a3e62694f4c0fe30ee6b7bd
jlu4170 <jlu@4170trading.com>
2018-04-23 15:05:06 -0500

    form test

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,14 +18,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
+                url: "http://www.gfru.it",
+                type: "GET",
                 cache: false,
                 success: function() {
                     // Success message
