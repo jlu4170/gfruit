c7f2f29be425e6c18c952f8ba6c040e0aca279bc
elee2 <39816546+elee2@users.noreply.github.com>
2019-03-21 11:00:26 -0400

    Create verify.js

---
+++ js/verify.js
@@ -0,0 +1,16 @@
+  var id = idensic.init(
+    // selector of the WebSDK container (see above)
+    '#idensic',
+    // configuration object (see the settings in demo)
+    {
+      // provide your clientId (can be seen in the demo)
+      "clientId": "YouClientId",
+      // maybe some additional parameters, see the Demo to see which ones, e.g.
+      "externalUserId": "someUserIdInYoursystem"
+    },
+    // function for the WebSDK callbacks
+     function (messageType, payload) {
+       // e.g. just logging the incoming messages
+       console.log('[IDENSIC DEMO] Idensic message:', messageType, payload);
+     }
+  )
