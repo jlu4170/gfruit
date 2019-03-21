6c2052a7d313f5d8783627d3e587ef953dcfa8a4
elee2 <39816546+elee2@users.noreply.github.com>
2019-03-21 11:07:54 -0400

    Update verify.js

--- js/verify.js
+++ js/verify.js
@@ -5,8 +5,7 @@
     {
       // provide your clientId (can be seen in the demo)
       "clientId": " Grapefruit Trading",
-      // maybe some additional parameters, see the Demo to see which ones, e.g.
-      "externalUserId": "someUserIdInYoursystem"
+      "requiredIdDocs": { "docSets": [ { "idDocSetType": "APPLICANT_DATA", "fields": [ { "name": "firstName", "required": true }, { "name": "lastName", "required": true } ] }, { "idDocSetType": "IDENTITY", "types": [ "ID_CARD", "PASSPORT", "DRIVERS" ] }, { "idDocSetType": "SELFIE", "types": [ "SELFIE" ] }, { "idDocSetType": "PROOF_OF_RESIDENCE", "types": [ "UTILITY_BILL" ], "fields": [ { "name": "street" }, { "name": "subStreet" }, { "name": "buildingNumber" }, { "name": "flatNumber" }, { "name": "town" }, { "name": "state" }, { "name": "postCode" }, { "name": "country" } ] }, { "idDocSetType": "INVESTABILITY", "types": [ "INCOME_SOURCE", "COMPANY_DOC" ] }, { "idDocSetType": "ACCREDITED_INVESTOR", "types": [ "INVESTOR_DOC" ] } ] }, "navConf": { "showWelcomeScreen": true }, "uiConf": { "customCss": "", "lang": "en", "steps": { "APPLICANT_REQUEST": { "title": "", "button": "" }, "CREATE_APPLICANT": { "instructions": "", "title": "" }, "APPLICANT_DATA": { "videoRequired": "disabled" }, "IDENTITY": { "title": "Identity document", "videoRequired": "disabled" }, "SELFIE": { "videoRequired": "disabled" }, "PROOF_OF_RESIDENCE": { "videoRequired": "disabled" }, "INVESTABILITY": { "videoRequired": "disabled" }, "ACCREDITED_INVESTOR": { "videoRequired": "disabled" }, "STATUS": { "pending": { "title": "", "instructions": "" }, "success": { "title": "", "instructions": "" } } } }
     },
     // function for the WebSDK callbacks
      function (messageType, payload) {
